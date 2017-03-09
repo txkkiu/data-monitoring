5b195c2003e268c1ad269311be0f09bc8c6a2c23
nick <kalteuxn@gmail.com>
2017-03-09 15:53:28 -0500

    replaced mongo implementation with Rocks web server, changed command line functions to reflect db flyweight allowing user to specify which db to apply all commands to

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from pymongo import MongoClient
-import sendgrid
+import smtplib
 import os
-from sendgrid.helpers.mail import *
 import datetime
 import getpass
 import cmd
 import pandas
 import pymongo
+import time
+import requests
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 '''
 authors: Koushik Krishnan, Nick Kalteux
 '''
 # Config
-sendgrid_key = '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'
 source_email = 'krishnan.koushik@gmail.com'
-dest_email = 'krishnan.koushik@gmail.com'
+dest_email = 'kalteuxn@gmail.com'
 
 client = MongoClient()
 db = client.primer
-sg = sendgrid.SendGridAPIClient(apikey=sendgrid_key)
 rows, columns = os.popen('stty size', 'r').read().split()
 pandas.set_option('display.width', int(columns))
 # COMMNANDS
@@ -26,31 +27,29 @@ pandas.set_option('display.width', int(columns))
 # DROP KEY 
 
 def send_email(t_type, history):
-    from_email = Email(source_email)
-    to_email = Email(dest_email)
+    msg = MIMEMultipart()
+    msg['From'] = source_email
+    msg['To'] = dest_email
     subject = 'DATA_ADDED' if t_type == 'CREATE' else 'DATA_UPDATED'
+    msg['Subject'] = subject
     body = 'User ' + history['user'] + ' has ' + t_type + 'D key = ' + history['key'] + ' with value = ' + history['value']
-    content = Content('text/plain', body)
-    mail = Mail(from_email, subject, to_email, content)
-    response = sg.client.mail.send.post(request_body=mail.get())
-    return response.status_code
+    msg.attach(MIMEText(body, 'plain'))
+    s = smtplib.SMTP('')
+    #s.connect()
+    s.sendmail(source_email, [dest_email], msg.as_string())
+    s.quit()
 
-
-def set_pair(key, value):
+def set_pair(key, value, rocksdb):
     data_in = {}
     data_in[str(key)] = str(value)
     data_in['id'] = str(key)
     t_type = 'CREATE'
     success = False
-    if db.data.find_one({'id':str(key)}) is not None:
-        post = db.data.find_one({'id':str(key)})
-        post[key] = value
-        post['id'] = key
-        db.data.save(post)
+    if server_get_value(key, rocksdb) is not "key not found":
         t_type = 'UPDATE'
-        success = True
+        success = server_set_value(key, value, rocksdb)
     else:
-        success = db.data.insert_one(data_in).acknowledged
+        success = server_set_value(key, value, rocksdb)
     if success:
         now = datetime.datetime.now()
         key_data = str(key)
@@ -64,8 +63,23 @@ def set_pair(key, value):
         history_in['type'] = t_type
         history_in['timestamp'] = now
         result_h = db.history.insert_one(history_in)
-        send_email(t_type, history_in)
+        #send_email(t_type, history_in)
         return 'success'
+
+def server_set_value(key, value, rocksdb):
+    url = "http://localhost:8080/set"
+
+    payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"value\"\r\n\r\n" + value + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"key\"\r\n\r\n"+ key + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"db\"\r\n\r\n" + rocksdb +"\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"      
+    headers = {
+        'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+        'cache-control': "no-cache",
+        }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+    if response.status_code == 200:
+        return True
+    else:
+        return False 
 
 def history(num_rows=-1):
     history = []
@@ -80,7 +94,13 @@ def history(num_rows=-1):
     df = df[['timestamp', 'type', 'user', 'key', 'value']]
     return df
 
-    
+def server_get_value(key , rocksdb):
+    url = "http://localhost:8080/get"
+    querystring = {"key":key, "db": rocksdb}
+    response = requests.request("GET", url, params=querystring)
+    if response.status_code == 500:
+        return "key not found"
+    return response.text  
 
 def get_value(key):
     if db.data.find_one({'id': key}) is not None:
@@ -92,26 +112,26 @@ def get_value(key):
 class KeyValueStore(cmd.Cmd):
     
     def do_set(self, line):
-        '''set [key], [value]
-        if the key exists, the value will be updated
+        '''set [key], [value], [db]
+        if the key exists in the specified db, the value will be updated
         otherwise a new key value pair will be created
         '''
-        if len(line.split(',')) < 2:
-            print('Error set takes 2 arguments "KEY, VALUE"')
+        if len(line.split(',')) != 3:
+            print('Error set takes 3 arguments "KEY, VALUE, DB"')
         else:
-            key, value = line.split(',')[0], line.split(',')[1]
-            print(set_pair(key, value))
+            key, value, rocksdb = "".join(line.split(" ")).split(',')
+            print(set_pair(key, value, rocksdb))
 
     def do_get(self, line):
-        '''get [key]
-        if the key exists, the value will be returned
+        '''get [key], [db]
+        if the key exists within the specified db, the value will be returned
         otherwise you will be notified that the key is absent
         '''
-        if len(line.split()) != 1:
-            print('Error get takes 2 arguments "KEY"')
+        if len(line.split(',')) != 2:
+            print('Error get takes 2 arguments "KEY, DB"')
         else:
-            print(get_value(line))
-
+            key, rocksdb = "".join(line.split(" ")).split(',')
+            print(server_get_value(key, rocksdb))
     def do_hist(self, line):
         '''hist (optional) [num_rows_back]
         retrieve the last $num_rows_back$ rows in the history log
